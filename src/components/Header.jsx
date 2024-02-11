1edfd0ccca644cbcd27b49ffcf75e55cd9e14f78
deeshnnasreyashex <deeshnna.s@hexeam.in>
2024-02-11 20:36:45 +0530

    adddarkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkMode from "./DarkMode";
 const Header = () => {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
@@ -10,10 +11,15 @@ const Header = () => {
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href='/' className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-1 rounded-lg">IMDB</span>
-        <span className="text-xl hidden sm:inline">Clone</span>
-      </Link>
+      <div className="flex gap-2 items-center">
+      <DarkMode/>
+        <Link href="/" className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-1 rounded-lg">
+            IMDB
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 };
