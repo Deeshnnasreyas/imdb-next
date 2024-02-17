27f7026c9f4d43200184d007db6163ecaaf19f4f
deeshnnasreyashex <deeshnna.s@hexeam.in>
2024-02-17 11:38:51 +0530

    loadererror

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function loading() {
   return (
     <div className="flex justify-center">
-      <Image src="Spinner.svg" alt="" />
+      <Image src="Spinner.svg" alt="Loading.." width={30} height={30} />
     </div>
   );
 }
--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -4,8 +4,6 @@ export default async function SearchPage({ params }) {
   const searchTerm = params.searchTerm;
   const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}
   &language=en-US&page=1&include_adult=false`);
- 
-  
   const data = await res.json();
   const results = data.results;
   return (
