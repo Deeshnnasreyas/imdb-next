a1c1689619c451aedcfd3bce4d5c88bbb9d5e690
deeshnnasreyashex <deeshnna.s@hexeam.in>
2024-02-17 10:09:34 +0530

    errorpage

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -27,7 +27,7 @@ export default async function Moviepage({ params }) {
         <p className="text-lg mb-3">{movie.overview}</p>
        
         <p className="mb-3"> <span className="font-semibold mr-1">Date Released:</span>{movie.release_date || movie.first_air_date}</p>
-        <p className="mb-3"> <span className="font-semibold mr-1">Rating:"</span>{movie.vote_count}</p>
+        <p className="mb-3"> <span className="font-semibold mr-1">Rating:</span>{movie.vote_count}</p>
       </div>
     </div>
   );
